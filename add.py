cd101ce5126908b7c382be6227f01b1aa0bc106c
Prabhakaranchandrasekar <prabha.karan2627@yahoo.in>
2024-06-23 11:16:29 +0530

    Add files via upload

--- add.py
+++ add.py
@@ -1,3 +1,6 @@
-def add(a,b):
-    c=a+b
-    print(c)
+def add():
+ a=10
+ b=20
+ c=a+b
+ print(c)
+add()
---
+++ get.py
@@ -0,0 +1,24 @@
+# Name=input("Enter your name:")
+# Age=int(input("Enter your age:"))
+# print("Name:",Name)
+# print("Age:",Age)
+#
+# detail={"Name:":"Raja","Age:":25,"Contact:":9962457545}
+# for x in detail:
+#     print(x,detail[x])
+
+
+# person=int(input("Enter the number of persons:"))
+# for x in range(1,person+1):
+#   Name=input("Enter your name:")
+#   Age=int(input("Enter your age:"))
+#   print("Name:",Name)
+#   print("Age:",Age)
+
+# person = int(input("Enter the number of persons: "))
+# for x in range(1, person + 1):
+#  Name = input(f"Enter name of person {x}: ")
+#  Age = int(input(f"Enter age of person {x}: "))
+#  print(f"Name: {Name}")
+#  print(f"Age: {Age}")
+
